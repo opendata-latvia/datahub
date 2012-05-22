667cfedacfe5222f97b692dd78d82832653c6a91
Raimonds Simanovskis <raimonds.simanovskis@gmail.com>
2012-05-22 17:21:50 +0300

    set locale from session, allow to switch locales for session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,24 @@
 class ApplicationController < ActionController::Base
   include ProjectPathHelper
+  before_filter :set_locale
 
   protect_from_forgery
-  
+
   protected
-  
+
   rescue_from CanCan::AccessDenied do |exception|
     logger.error "[cancan] Access denied, message: #{exception.message}"
     redirect_to root_url, :alert => exception.message
   end
-  
-  def must_be_super_admin
-    authorize!(:manage, "Administration")
+
+  private
+
+  VALID_LOCALES = %w(lv en)
+
+  def set_locale
+    if VALID_LOCALES.include? params[:locale]
+      session[:locale] = params[:locale].to_sym
+    end
+    I18n.locale = session[:locale] || I18n.default_locale
   end
-  
 end
