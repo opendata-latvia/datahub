55df349bb80a45c414ea5c79e6f2dc70bad24154
Raimonds Simanovskis <raimonds.simanovskis@gmail.com>
2012-06-04 13:47:51 +0300

    added project search specs

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -52,4 +52,40 @@ describe Project do
     end
 
   end
+
+  describe "search" do
+    before(:all) do
+      Project.destroy_all
+    end
+
+    before(:each) do
+      @project_foo_bar = create(:project, :name => "foo bar", :description => "lorem ipsum")
+    end
+
+    it "should return all projects when no search query" do
+      Project.search(:q => "").should == Project.all
+    end
+
+    it "should return projects with matching name" do
+      Project.search(:q => "foo").should == [@project_foo_bar]
+    end
+
+    it "should not return any project when no matching name" do
+      Project.search(:q => "baz").should be_empty
+    end
+
+    it "should return projects with several matching terms" do
+      Project.search(:q => "bar foo").should == [@project_foo_bar]
+    end
+
+    it "should not return projects with matching and not matching terms" do
+      Project.search(:q => "foo baz").should be_empty
+    end
+
+    it "should return projects with matching description" do
+      Project.search(:q => "lorem").should == [@project_foo_bar]
+    end
+
+  end
+
 end
